eb4ecbe2b4ef593d65b43f35b72c15ad733a63d2
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2019-01-29 14:10:04 +0200

    Added the translate function in common.php.

--- public/common.php
+++ public/common.php
@@ -13,6 +13,10 @@ if(!isset($_SESSION['cart'])){
     $_SESSION['cart'] = array();
 }
 
+function translate($text){
+    return $text;
+}
+
 $conn = new mysqli(DBHOST, DBUSER, DBPWD, DBNAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ if(!isset($link_address1)) {
 $sql = "SELECT id, title, description, price FROM products";
 $result = $conn->query($sql);
 
+
 ?>
 
 <html>
@@ -18,12 +19,12 @@ $result = $conn->query($sql);
     </head>
     <body>
         <h1>
-            Produse
+            <?php echo translate("Products") ?>
         </h1>
             <?php if ($result->num_rows > 0): ?>
                 <table>
                     <?php while ($row = $result->fetch_assoc()): ?>
-                        <?php if(!array_search($row["id"], $_SESSION['cart'])): ?>
+                        <?php if(!array_search($row["id"], $_SESSION["cart"])): ?>
                             <tr>
                                 <td class="cp_img">
                                     <img src="img/<?=$row["id"]?>.jpg"/>
@@ -34,7 +35,7 @@ $result = $conn->query($sql);
                                         <li><?=$row["description"]?></li>
                                         <li><?=$row["price"]?></li></ul></td>
                                 <td>
-                                    Add
+                                    <?php echo translate("Add") ?>
                                 </td>
                             </tr>
                         <?php else: ?>
@@ -43,6 +44,6 @@ $result = $conn->query($sql);
             <?php endif; ?>
             <?php $conn->close(); ?>
                 </table>
-        <a href=<?=$link_address1?>>Go to cart</a>
+        <a href=<?=$link_address1?>> <?php echo translate("Go to cart") ?></a>
     </body>
 </html>
