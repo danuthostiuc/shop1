0681c243e481ae0307a523ee2d216c313faf5487
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2019-01-29 14:00:05 +0200

    finished displaying all product properties.

--- public/common.php
+++ public/common.php
@@ -12,32 +12,9 @@ if(!isset($_SESSION['cart'])){
     //If it doesn't, create an empty array.
     $_SESSION['cart'] = array();
 }
-if(!isset($link_address1)) {
-    $link_address1 = 'cart.php';
-}
 
 $conn = new mysqli(DBHOST, DBUSER, DBPWD, DBNAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, title, description, price FROM products";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    echo "<table>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        if(!array_search($row["id"], $_SESSION['cart'])) {
-            echo "<tr><td><img src='img/".$row["id"].".jpg'/></td><td><ul><li>" . $row["title"] . "</li><li>" . $row["description"] . "</li><li>" . $row["price"] . "</li></ul></td><td>Add</td></tr>";
-        }
-    }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
-
-echo "<a href='".$link_address1."'>Go to cart</a>";
-
-$conn->close();
--- public/index.php
+++ public/index.php
@@ -1,3 +1,48 @@
 <?php
 require_once('common.php');
+session_start();
 
+if(!isset($link_address1)) {
+    $link_address1 = 'cart.php';
+}
+
+
+$sql = "SELECT id, title, description, price FROM products";
+$result = $conn->query($sql);
+
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <h1>
+            Produse
+        </h1>
+            <?php if ($result->num_rows > 0): ?>
+                <table>
+                    <?php while ($row = $result->fetch_assoc()): ?>
+                        <?php if(!array_search($row["id"], $_SESSION['cart'])): ?>
+                            <tr>
+                                <td class="cp_img">
+                                    <img src="img/<?=$row["id"]?>.jpg"/>
+                                </td>
+                                <td>
+                                    <ul>
+                                        <li><?=$row["title"]?></li>
+                                        <li><?=$row["description"]?></li>
+                                        <li><?=$row["price"]?></li></ul></td>
+                                <td>
+                                    Add
+                                </td>
+                            </tr>
+                        <?php else: ?>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
+            <?php endif; ?>
+            <?php $conn->close(); ?>
+                </table>
+        <a href=<?=$link_address1?>>Go to cart</a>
+    </body>
+</html>
