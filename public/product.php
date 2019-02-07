fc6f4d908af9660661cae6d42f1b31ce64720432
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2019-02-07 18:00:30 +0200

    - rewrote catch for UPDATE statement.

--- public/product.php
+++ public/product.php
@@ -40,6 +40,8 @@ if (isset($_POST["save"]) && isset($_GET["id"])) {
         try {
             $stmt = $conn->prepare("UPDATE products SET title=?, description=?, price=?, image=? WHERE id=?");
             $stmt->execute([$title, $description, $price, $_FILES["image"]["name"], $_GET["id"]]);
+        } catch (PDOException $e) {
+            $php_errormsg = translate("Error: " . $e->getMessage());
         } finally {
             header("Location: products.php");
             die;
