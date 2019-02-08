3f82573d392f339cb7f43632f5ae1a23552d104f
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2019-02-08 12:02:09 +0200

    - added query and html table.

--- public/orders.php
+++ public/orders.php
@@ -6,26 +6,58 @@
  * Time: 11:26 AM
  */
 require_once("common.php");
-var_dump($_SESSION["orders"]);
+
+if (!isset($_SESSION["admin"])) {
+    header("Location: login.php");
+    die;
+}
+
+try {
+    $stmt = $conn->prepare("SELECT name, contact, comment, SUM(p.price) AS 'total' FROM orders AS o
+                            JOIN prod_ord AS po ON o.id = po.ord_id
+                            JOIN products AS p ON po.prod_id = p.id
+                            GROUP BY o.id");
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $orders = $stmt->fetchAll();
+} catch (PDOException $e) {
+    $php_errormsg = translate("Error: " . $e->getMessage());
+}
+
 ?>
 
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="<?= CSS_PATH ?>">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 <h1>
     <?= translate("Orders") ?>
 </h1>
 
-<?php foreach ($_SESSION["orders"] as $order): ?>
-    <?php if (!$order instanceof Order): ?>
-        <?php continue; ?>
-    <?php endif; ?>
-    <?= $order->displayOrder(); ?>
-<?php endforeach; ?>
+<?php if (!empty($err_conn_database) || !empty($err_select)) : ?>
+    <?= $err_conn_database ?>
+    <?= $php_errormsg ?>
+<?php else: ?>
+    <table>
+        <?php foreach ($orders as $row): ?>
+            <tr>
+                <td>
+                    <?= $row["name"] ?>
+                </td>
+                <td>
+                    <?= $row["contact"] ?>
+                </td>
+                <td>
+                    <?= $row["comment"] ?>
+                </td>
+                <td>
+                    <?= $row["total"] ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+<?php endif; ?>
 
-<a href="index.php"> <?= translate("Go to index") ?></a>
-<a href="login.php"> <?= translate("Log In") ?></a>
 </body>
 </html>
