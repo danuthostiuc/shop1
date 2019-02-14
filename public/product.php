b623bcf149c0466607d3e0b8ce9089fb4558ee68
Danut Hostiuc <danut.hostiuc@5psolutions.eu>
2019-02-14 17:03:34 +0200

    - the form is keeping its view stat in case of validation fail for the add branch; - the image names are unique; - changed the structure of the is clauses.

--- public/product.php
+++ public/product.php
@@ -12,56 +12,63 @@ if (!isset($_SESSION["admin"])) {
     die;
 }
 
-if (isset($_POST["title"]) && isset($_POST["description"]) && isset($_POST["price"]) && isset($_FILES["image"]["name"])
-    && !isset($_GET["id"])) {
-    if (!empty($_POST["title"]) && !empty($_POST["description"]) && !empty($_POST["price"])
-        && !empty($_FILES["image"]["name"])) {
+if (isset($_POST["title"]) && isset($_POST["description"]) && isset($_POST["price"])) {
+    if (!empty($_POST["title"]) && !empty($_POST["description"]) && !empty($_POST["price"])) {
+
         $title = testInput($_POST["title"]);
         $description = testInput($_POST["description"]);
         $price = testInput($_POST["price"]);
-        $image_file_type = strtolower(pathinfo($_FILES["image"]["name"],PATHINFO_EXTENSION));
-        $extensions_arr = array("png","gif","jpeg","jpg");
-        if (in_array($image_file_type, $extensions_arr)) {
-            try {
-                $stmt = $conn->prepare("INSERT INTO products(title, description, price, image) 
+
+        if (!isset($_GET["id"]) && isset($_FILES["image"]["name"]) && !empty($_FILES["image"]["name"])) {
+            $unique = uniqid();
+            $name = $_FILES["image"]["name"];
+            $target_dir = "img/";
+            $target_file = $target_dir . basename($_FILES["image"]["name"]);
+            $image_file_type = strtolower(pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION));
+            $extensions_arr = array("png", "gif", "jpeg", "jpg");
+            if (in_array($image_file_type, $extensions_arr)) {
+                try {
+                    $stmt = $conn->prepare("INSERT INTO products(title, description, price, image) 
                                     VALUES (:title, :description, :price, :image)");
-                $stmt->execute(array(':title' => $title, ':description' => $description, ':price' => $price,
-                    ':image' => $_FILES["image"]["name"]));
+                    $stmt->execute(array(':title' => $title, ':description' => $description, ':price' => $price,
+                        ':image' => $unique . "." . $image_file_type));
+                    if (file_exists($target_dir . $name)) {
+                        rename($target_dir . $name, $target_dir . $unique . "." . $image_file_type);
+                    } else {
+                        move_uploaded_file($_FILES["image"]["tmp_name"], $target_dir . $name);
+                        rename($target_dir . $name, $target_dir . $unique . "." . $image_file_type);
+                    }
+                } catch (PDOException $e) {
+                    $php_errormsg = sprintf(translate("Error: %s") . $e->getMessage());
+                } finally {
+                    header("Location: products.php");
+                    die;
+                }
+            } else {
+                $php_errormsg = translate("Invalid file format!");
+            }
+        }
+
+        if (isset($_GET["id"]) && !isset($_FILES["image"]["name"]) && empty($_FILES["image"]["name"])) {
+            try {
+                $stmt = $conn->prepare("UPDATE products SET title=?, description=?, price=? WHERE id=?");
+                $stmt->execute([$title, $description, $price, $_GET["id"]]);
             } catch (PDOException $e) {
-                $php_errormsg = sprintf(translate("Error: %s") . $e->getMessage());
-            } finally {
+                $php_errormsg = sprintf(translate("Error: %s"), $e->getMessage());
+            }
+            finally {
                 header("Location: products.php");
                 die;
             }
-        } else {
-            $php_errormsg = translate("Invalid file format!");
         }
     } else {
         $php_errormsg = translate("Complete all fields!");
     }
 }
 
-if (isset($_POST["title"]) && isset($_POST["description"]) && isset($_POST["price"]) && isset($_GET["id"])) {
-    if (!empty($_POST["title"]) && !empty($_POST["description"]) && !empty($_POST["price"])) {
-        $title = testInput($_POST["title"]);
-        $description = testInput($_POST["description"]);
-        $price = testInput($_POST["price"]);
-        try {
-            $stmt = $conn->prepare("UPDATE products SET title=?, description=?, price=? WHERE id=?");
-            $stmt->execute([$title, $description, $price, $_GET["id"]]);
-        } catch (PDOException $e) {
-            $php_errormsg = sprintf(translate("Error: %s"), $e->getMessage());
-        } finally {
-            header("Location: products.php");
-            die;
-        }
-    } else {
-        $php_errormsg = translate("Complete all fields!");
-    }
-}
+$product = [];
 
 if (isset($_GET["id"])) {
-    $product = [];
     try {
         $stmt = $conn->prepare("SELECT * FROM products WHERE id=?");
         $stmt->bindValue(1, $_GET["id"], PDO::PARAM_INT);
@@ -71,8 +78,8 @@ if (isset($_GET["id"])) {
     } catch (PDOException $e) {
         $php_errormsg = sprintf(translate("Error: %s"), $e->getMessage());
     }
-
 }
+
 ?>
 
 <html>
@@ -83,21 +90,33 @@ if (isset($_GET["id"])) {
 <h1>
     <?= translate("Product") ?>
 </h1>
+
 <?php if (!empty($php_errormsg)): ?>
     <?= $php_errormsg ?>
 <?php endif; ?>
+
 <form method="post" enctype="multipart/form-data">
-    <input type="text" name="title" <?php if (isset($_GET["id"])): ?>
-                                    value="<?= $product[0]["title"] ?>"
-                                    <?php endif; ?> placeholder="<?= translate("Title") ?>">
+    <input type="text" name="title"
+        <?php if (isset($_GET["id"])): ?>
+            value="<?= $product[0]["title"] ?>"
+        <?php else: ?>
+            value="<?= isset($_POST["title"]) ? htmlentities($_POST["title"]) : "" ?>"
+        <?php endif; ?>
+           placeholder="<?= translate("Title") ?>">
     <br>
-    <input type="text" name="description" <?php if (isset($_GET["id"])): ?>
-                                          value="<?= $product[0]["description"] ?>"
-                                          <?php endif; ?> placeholder="<?= translate("Description") ?>">
+    <input type="text" name="description"
+        <?php if (isset($_GET["id"])): ?>
+            value="<?= $product[0]["description"] ?>"
+        <?php else: ?>
+            value="<?= isset($_POST["description"]) ? htmlentities($_POST["description"]) : "" ?>"
+        <?php endif; ?> placeholder="<?= translate("Description") ?>">
     <br>
-    <input type="number" name="price" <?php if (isset($_GET["id"])): ?>
-                                      value="<?= $product[0]["price"] ?>"
-                                      <?php endif; ?> placeholder="<?= translate("Price") ?>">
+    <input type="number" name="price"
+        <?php if (isset($_GET["id"])): ?>
+            value="<?= $product[0]["price"] ?>"
+        <?php else: ?>
+            value="<?= isset($_POST["price"]) ? htmlentities($_POST["price"]) : "" ?>"
+        <?php endif; ?> placeholder="<?= translate("Price") ?>">
     <br>
     <?php if (!isset($_GET["id"])): ?>
         <input type="file" name="image" accept=".png, .gif, .jpeg, .jpg">
